dfc1d3a1f7e9dbf16fbc5e3aac75fa7038b0e37c
Shahzad Hashmi <Shashmi154@gmail.com>
2022-05-09 14:42:46 +0500

    setting form values using formbuilder

--- simpleCRM/src/app/loan-types/loan-types.component.ts
+++ simpleCRM/src/app/loan-types/loan-types.component.ts
@@ -25,23 +25,65 @@ export class LoanTypesComponent implements OnInit {
     });
     */
 
-  /*
-  Now by using formBuilder, which provides us three things
-  1: FormGroup
-  2: FormArray
-  3: FormControls
-  */
+    /*
+    Now by using formBuilder, which provides us three things
+    1: FormGroup
+    2: FormArray
+    3: FormControls
+    */
+
+    /* this.addLoanTypesForm = this.formBuilder.group({
+       'loanName': new FormControl(),
+       'loanType': new FormControl(),
+       'loanDescription': new FormControl(),
+     })
+     */
+
+    /*
+    Now setting form values
+    we have two ways to set values in formBuilder
+    1: set the value of entire form in one go
+    2: setting the form value using setValue method(by creating object)
+     */
+
+    /*
+        Now setting form values
+        we have two ways to set values in formBuilder
+        1: set the value of entire form in one go
+        2: setting the form value using setValue method(by creating object)
+         */
+
+    /*
+    // Method 1:set the value of entire form in one go
+    this.addLoanTypesForm = this.formBuilder.group({
+      'loanName': new FormControl('Shahzad Loan'),
+      'loanType': new FormControl('Bank Loan'),
+      'loanDescription': new FormControl('Taking loan from bank for business'),
+    });
+    */
+
+    /* 
+    Method2: setting the form value using setValue method(by creating object)
+    */
 
     this.addLoanTypesForm = this.formBuilder.group({
       'loanName': new FormControl(),
       'loanType': new FormControl(),
       'loanDescription': new FormControl(),
-    })
+    }); 
+
+    const newLoanObj = {
+      'loanName': 'Shahzad Loan',
+      'loanType': 'Personal Loan',
+      'loanDescription': 'Taking Personal loan from bank for study',
+    }
+
+    this.addLoanTypesForm.setValue(newLoanObj);
+
   }
 
-  addLoanType(){
+  addLoanType() {
     console.log(this.addLoanTypesForm.value);
   }
 
 }
- 
